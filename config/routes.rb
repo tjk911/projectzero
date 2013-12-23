8c3f0837bd59c1b9a7883d53d33da47a8cdf0dcb
Kai Teoh <kai@ink-cafe.com>
2013-12-23 13:55:00 -0600

    testing a new Books db

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Projectzero::Application.routes.draw do
+  resources :books
+
   root :to => 'hello#sayhi'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
